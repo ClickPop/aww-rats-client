3da190a7dd5ad7e9a37a669e439ac7d71a16ea33
Graham Vasquez <rescus1221@gmail.com>
2021-09-15 23:20:06 -0400

    Added update contract meta URI task to hardhat

--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -8,13 +8,26 @@ import "@nomiclabs/hardhat-ethers";
 import "tsconfig-paths/register";
 import "hardhat-watcher";
 
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, task } from "hardhat/config";
 import {
   MUMBAI_TESTNET,
   MUMBAI_TESTNET_CHAIN_ID,
   PRIVATE_KEY_ADMIN,
   PRIVATE_KEY_USER,
+  CONTRACT_ADDRESS
 } from "./src/config/env";
+
+task("update-contract-uri", "Script to update the contract metadata uri for our collection on OpenSea").addPositionalParam("uri", "The new metadata URI that will be set on the smart-contract").setAction(async ({uri}, hre) => {
+  try {
+    const signer = await hre.ethers.getSigner(PRIVATE_KEY_ADMIN ?? "")
+    const Rat = await hre.ethers.getContractFactory("Rat", signer)
+    const rat = Rat.attach(CONTRACT_ADDRESS ?? "")
+    const tx = await rat.setContractURI(uri).then(t => t.wait());
+    console.log("Transaction Hash:", tx.transactionHash);
+  } catch (err) {
+    console.error(err)
+  }
+})
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
