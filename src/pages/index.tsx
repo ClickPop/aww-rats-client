9077016ebfe7ac115f923a13b291e8ec3f08f7bc
chris <chrisvqz@gmail.com>
2021-09-25 13:59:16 -0400

    Update roadmap and replace roadmap page with roadmap section.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { Minter } from '~/components/minting/Minter';
 import { Hero } from '~/components/index/hero/Hero';
 import { About } from '~/components/index/about/About';
 import { Roadmap } from '~/components/index/roadmap/Roadmap';
+import { FAQ } from '~/components/index/faq/FAQ';
 import { RatRace } from '~/components/index/rat-race/RatRace';
 import { Layout } from '~/components/layout/Layout';
 
@@ -15,6 +16,7 @@ const Home: NextPage = () => {
       </div>
       <About />
       <Roadmap />
+      <FAQ />
       <RatRace />
     </Layout>
   )
