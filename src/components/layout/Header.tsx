26bdc8f9c464922fbe39f27f90b03f7c923bcaf0
chris <chrisvqz@gmail.com>
2021-09-25 14:16:40 -0400

    Update nav

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Image } from '~/components/shared/Image'
+import { Link } from '~/components/shared/Link'
 import logo from '~/assets/images/aww-rats-logo.png'
 import discord from '~/assets/svg/discord-logo.svg'
 import twitter from '~/assets/svg/twitter-logo.svg'
@@ -9,9 +10,15 @@ export const Header = () => {
   return (
     <div className="pt-4 w-full">
       <Image className="w-full h-28 py-1 px-16" src={logo} layout="fill" objectFit="contain" alt="Aww, Rats! Logo" placeholder="blur" />
-      <div className="mt-3 w-fit mx-auto flex justify-center items-center space-x-5">
-        <Image src={twitter} alt="Discord Logo" width={32} height={32} />
-        <Image src={discord} alt="Discord Logo" width={32} height={32} />
+      <div className="flex max-w-2xl mx-auto align-center justify-between content-center px-4">
+        <div className="flex items-center">
+          <Link href="#roadmap" className="text-white p-2 mr-2">Roadmap</Link>
+          <Link href="#faqs" className="text-white p-2">FAQs</Link>
+        </div>
+        <div className="flex justify-center items-center space-x-5">
+          <Link href="https://www.twitter.com/awwratspack"><Image src={twitter} alt="Discord Logo" width={32} height={32} /></Link>
+          <Link href="https://discord.gg/2cwxkBkgf5"><Image src={discord} alt="Discord Logo" width={32} height={32} /></Link>
+        </div>
       </div>
     </div>
   )
