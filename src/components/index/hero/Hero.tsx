86bb48ea18dc48dea4ecd4b8f2aa7cd521f2173e
chris <chrisvqz@gmail.com>
2021-09-25 14:54:37 -0400

    Move minter to the hero component

--- src/components/index/hero/Hero.tsx
+++ src/components/index/hero/Hero.tsx
@@ -1,14 +1,53 @@
 import React from 'react'
+import { ethers, utils, BigNumber } from 'ethers';
+import { CONTRACT_ADDRESS } from '~/config/env';
+import RatABI from "smart-contracts/artifacts/src/contracts/Rat.sol/Rat.json";
+import { Rat } from '~/types';
+import { useEffect, useState } from 'react';
+import { useEthers } from '~/hooks/useEthers';
 import { Image } from '~/components/shared/Image'
 import rats from '~/assets/images/rats.gif'
+import { Minter } from '~/components/minting/Minter';
 
 export const Hero = () => {
+
+  const [metamaskConn, setMetamaskConn] = useState(false);
+  const [ethCost, setEthCost] = useState(0);
+  const [contract, setContract] = useState<Rat | null>(null);
+  const { provider, signer } = useEthers();
+
+  const connectToMetamask = async () => {
+    try {
+      await provider?.send("eth_requestAccounts", []);
+      setMetamaskConn(true);
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  useEffect(() => {
+    (async () => {
+      if (CONTRACT_ADDRESS && metamaskConn) {
+        const c = new ethers.Contract(CONTRACT_ADDRESS, RatABI.abi, signer) as Rat
+        setContract(c);
+        c.cost().then(data => {
+          setEthCost(parseFloat(utils.formatEther(data)));
+        })
+      }
+    })();
+  }, [metamaskConn, signer]);
+
   return (
     <div className="pb-16 text-white w-full">
-      <div className="text-center flex flex-col justify-center items-center w-1/2 max-w-xl h-full mx-auto">
-          <Image className="overflow-hidden rounded-full w-80 h-80 mt-3 mb-8" src={rats} layout="fill" objectFit="contain" alt="Rotating gif displaying some rats" />
-          <h1 className="text-4xl px-10 mb-2 font-bold">An NFT Project By Creators, for Creators.</h1>
-          <p className="px-2 text-lg">We&apos;re sharing the tools we built and the skills we learned with all of our rat holders so more artists can make generative art and NFTs.</p>
+      <div className="text-center grid md:grid-cols-3 justify-center items-center max-w-4xl h-full mx-auto">
+        <Image className="imgfix overflow-hidden rounded-full mt-3 mb-8" src={rats} alt="Rotating gif displaying some rats" />
+        <div className="px-4 md:col-span-2 md:text-left">
+          <h1 className="text-4xl mb-2 font-bold">An NFT Project By Creators, for Creators.</h1>
+          <p className="text-lg">We&apos;re sharing the tools we built and the skills we learned with all of our rat holders so more artists can make generative art and NFTs.</p>
+          <div className="text-light w-fit mx-auto">
+            {metamaskConn ? <Minter ethCost={ethCost} contract={contract} /> : <button onClick={connectToMetamask}>Connect to metamask</button>}
+          </div>
+        </div>
       </div>
       {/* {metamaskConn ? <Minter ethCost={ethCost} contract={contract} /> : <button onClick={connectToMetamask}>Connect to metamask</button>} */}
     </div>
