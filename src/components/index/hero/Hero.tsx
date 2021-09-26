19c2ff246c523cdfcf4e0b1f1880683f752ffa3d
chris <chrisvqz@gmail.com>
2021-09-25 22:38:09 -0400

    Fix hero section

--- src/components/index/hero/Hero.tsx
+++ src/components/index/hero/Hero.tsx
@@ -6,7 +6,13 @@ import { Rat } from '~/types';
 import { useEffect, useState } from 'react';
 import { useEthers } from '~/hooks/useEthers';
 import { Image } from '~/components/shared/Image'
-import rats from '~/assets/images/rats.gif'
+import rat01 from '~/assets/images/rats/rat-01.png'
+import rat02 from '~/assets/images/rats/rat-02.png'
+import rat03 from '~/assets/images/rats/rat-03.png'
+import rat04 from '~/assets/images/rats/rat-04.png'
+import rat05 from '~/assets/images/rats/rat-05.png'
+import rat06 from '~/assets/images/rats/rat-06.png'
+import rat07 from '~/assets/images/rats/rat-07.png'
 import { Minter } from '~/components/minting/Minter';
 
 export const Hero = () => {
@@ -38,17 +44,25 @@ export const Hero = () => {
   }, [metamaskConn, signer]);
 
   return (
-    <div className="pb-16 text-white w-full">
-      <div className="text-center grid md:grid-cols-3 justify-center items-center max-w-4xl h-full mx-auto">
-        <Image className="imgfix overflow-hidden rounded-full mt-3 mb-8" src={rats} alt="Rotating gif displaying some rats" />
-        <div className="px-4 md:col-span-2 md:text-left">
-          <h1 className="text-4xl mb-2 font-bold">An NFT Project By Creators, for Creators.</h1>
-          <p className="text-lg">We&apos;re sharing the tools we built and the skills we learned with all of our rat holders so more artists can make generative art and NFTs.</p>
-          <div className="text-light w-fit mx-auto">
-            <div className="text-light w-fit mx-auto">
-              <Minter />
-            </div>
-          </div>
+    <div className="pb-16 pt-12 overflow-hidden text-white w-full">
+      <div className="text-center justify-center items-center max-w-4xl mx-auto px-4">
+        <h1 className="text-4xl mb-2 font-bold">An NFT Project By Creators, for Creators.</h1>
+      </div>
+      <div className="w-screen my-12 overflow-hidden">
+        <div className="flex flex-row items-center justify-center overflow-hidden">
+          <Image className="ratvatar imgfix overflow-hidden rounded-full mx-8" src={rat01} alt="" />
+          <Image className="ratvatar imgfix overflow-hidden rounded-full mx-8" src={rat02} alt="" />
+          <Image className="ratvatar imgfix overflow-hidden rounded-full mx-8" src={rat03} alt="" />
+          <Image className="ratvatar ratvatar-lg imgfix overflow-hidden rounded-full mx-8" src={rat04} alt="" />
+          <Image className="ratvatar imgfix overflow-hidden rounded-full mx-8" src={rat05} alt="" />
+          <Image className="ratvatar imgfix overflow-hidden rounded-full mx-8" src={rat06} alt="" />
+          <Image className="ratvatar imgfix overflow-hidden rounded-full mx-8" src={rat07} alt="" />
+        </div>
+      </div>
+      <div className="text-center justify-center items-center max-w-xl mx-auto px-4">
+        <p className="text-lg">We&apos;re sharing the tools we built and the skills we learned with all of our rat holders so more artists can make generative art and NFTs.</p>
+        <div className="text-light w-fit mx-auto">
+          <Minter />
         </div>
       </div>
     </div>
