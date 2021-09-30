e0ef33db07854d1a8ae8e45b1e29f6e96fab2518
Graham Vasquez <rescus1221@gmail.com>
2021-09-30 03:06:36 -0400

    Added default metadata

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/default-metadata/:test*",
+        destination: "/default-erc721-token-metadata.json",
+        permanent: true
+      }
+    ]
+  },
 }
