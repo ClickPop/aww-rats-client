648111d8677cd54b7811f660d36a13dd8f423c16
Graham Vasquez <rescus1221@gmail.com>
2021-10-07 06:40:55 -0400

    Added prettier and husky

--- src/components/shared/svg/WhiteSwoosh.tsx
+++ src/components/shared/svg/WhiteSwoosh.tsx
@@ -1,16 +1,15 @@
-import { FC, SVGProps } from "react"
+import { FC, SVGProps } from 'react';
 
 export const WhiteSwoosh: FC<SVGProps<SVGSVGElement>> = (props) => {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      x="0px"
-      y="0px"
-      viewBox="0 0 1920 72.4"
-      xmlSpace="preserve"
-      {...props}
-    >
-      <path d="M1940 0s-365.9 48-980 48S-20 0-20 0v72.4h1960V0z" fill="#fff" />
+      xmlns='http://www.w3.org/2000/svg'
+      x='0px'
+      y='0px'
+      viewBox='0 0 1920 72.4'
+      xmlSpace='preserve'
+      {...props}>
+      <path d='M1940 0s-365.9 48-980 48S-20 0-20 0v72.4h1960V0z' fill='#fff' />
     </svg>
-  )
-}
+  );
+};
