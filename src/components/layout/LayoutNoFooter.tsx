3143e62f95c8e7caf61948274ff20fc496bd1fdc
Graham Vasquez <rescus1221@gmail.com>
2021-10-12 03:07:29 -0400

    Fixed canvas scaling issues

--- src/components/layout/LayoutNoFooter.tsx
+++ src/components/layout/LayoutNoFooter.tsx
@@ -1,7 +1,10 @@
 import React, { FC } from 'react';
 import { Header } from '~/components/layout/Header';
 
-export const LayoutNoFooter: FC<{ className?: string }> = ({ children, className }) => {
+export const LayoutNoFooter: FC<{ className?: string }> = ({
+  children,
+  className,
+}) => {
   return (
     <div className={`${className ?? ''} w-full`}>
       <Header />
