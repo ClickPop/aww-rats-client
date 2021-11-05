3cc8f4e4747d41b200e7b7c18c082b62d41bf0bd
Sean Metzgar <sean.metzgar@gmail.com>
2021-11-04 23:20:58 -0400

    Adds basic Study page

---
+++ src/components/study/Study.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Study = () => {
+  return (
+    <div className='w-max-96 w-full text-center'>
+      <ul className='text-light'>
+        <li>List of files...</li>
+        <li>Goes here...</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Study;
