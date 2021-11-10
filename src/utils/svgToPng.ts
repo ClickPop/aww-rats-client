c44f35ea0f0f2876e2d9c1d579a66d8fa5405733
Sean Metzgar <sean.metzgar@gmail.com>
2021-11-10 02:46:05 -0500

    Now loads SVG data URIs

---
+++ src/utils/svgToPng.ts
@@ -0,0 +1,29 @@
+export const svgToPng = (svgDataURI: string): Promise<string> => new Promise((resolve) => {
+  let canvas: HTMLCanvasElement;
+  let ctx: CanvasRenderingContext2D | null;
+  let img: HTMLImageElement;
+  let bounds: number = 2048;
+
+  img = new Image();
+  img.src= svgDataURI;
+  img.width = bounds
+  img.height = bounds
+  img.onload = () => {
+      canvas = document.createElement('canvas');
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx = canvas.getContext('2d');
+      ctx?.drawImage(img, 0, 0, img.width, img.height, 0, 0, bounds, bounds);
+
+      img = new Image();
+      img.src = canvas.toDataURL('image/png');
+      img.onload = () => {
+          canvas = document.createElement('canvas');
+          canvas.width = bounds;
+          canvas.height = bounds;
+          ctx = canvas.getContext('2d');
+          ctx?.drawImage(img, 0, 0);
+          resolve(canvas.toDataURL('image/png'));
+      }
+  };
+});
