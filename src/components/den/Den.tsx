ae3a514ac43b8b2dbe28e024cfac71bd7e9a9070
Sean Metzgar <sean.metzgar@gmail.com>
2021-11-10 10:30:34 -0500

    Whoops… take that thing out of there…

--- src/components/den/Den.tsx
+++ src/components/den/Den.tsx
@@ -223,12 +223,10 @@ const Den = () => {
     const getTokens = async () => {
       if (signerAddr) {
         const resPolygon = await fetch(
-          // `/api/get-tokens/${signerAddr'}?chain=polygon`,
-          `/api/get-tokens/${'0x874fa6cd927d748326d5004065636e343df97405'}?chain=polygon`,
+          `/api/get-tokens/${signerAddr'}?chain=polygon`,
         ).then((r) => r.json());
         const resEthereum = await fetch(
-          // `/api/get-tokens/${signerAddr}?chain=eth`,
-          `/api/get-tokens/${'0x874fa6cd927d748326d5004065636e343df97405'}?chain=eth`,
+          `/api/get-tokens/${signerAddr}?chain=eth`,
         ).then((r) => r.json());
         setTokens([...resPolygon.data, ...resEthereum.data]);
       }
