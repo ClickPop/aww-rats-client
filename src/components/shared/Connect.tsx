ef90edbcce795a126cc596020a21812eb17afa13
Sean Metzgar <sean.metzgar@gmail.com>
2021-11-14 23:14:38 -0500

    Add mumbai data and refactor functionality to prevent massive memory leak

--- src/components/shared/Connect.tsx
+++ src/components/shared/Connect.tsx
@@ -10,31 +10,78 @@ interface NetworkSwitchError {
   error?: any;
 }
 
+interface ChainCurrency {
+  name: string;
+  symbol: string;
+  decimals: number;
+}
+interface ChainData {
+  id: number;
+  name: string;
+  nativeCurrency: ChainCurrency | null;
+  rpc: string[] | string;
+  scan: string[] | string;
+}
+
 export const Connect = () => {
   const { provider, network, connected } = useEthers();
   const [addNetworkActive, setAddNetworkActive] = useState<boolean>(false);
-  const [chainName, setChainName] = useState<string>('');
-  const [switchChainError, setSwitchChainError] =
-    useState<NetworkSwitchError | null>(null);
+  const [chainData, setChainData] = useState<ChainData | null>(null);
+  const [
+    switchChainError,
+    setSwitchChainError,
+  ] = useState<NetworkSwitchError | null>(null);
 
   useEffect(() => {
-    let chain_name = '';
-    let active = true;
+    let chain_data: ChainData | null = null;
+    let chain_name: string = '';
+    let chain_currency: ChainCurrency | null = null;
+    let chain_rpc: string[] = [];
+    let chain_scan: string[] = [];
+    let active: boolean = true;
 
     switch (CHAIN_ID) {
       case 137:
         chain_name = 'Polygon Mainnet';
+        chain_currency = {
+          name: 'MATIC',
+          symbol: 'MATIC',
+          decimals: 18,
+        };
+        chain_rpc = [
+          'https://polygon-rpc.com/',
+          'https://rpc-mainnet.matic.network/',
+          'https://rpc-mainnet.maticvigil.com/',
+          'https://rpc-mainnet.matic.quiknode.pro/',
+        ];
+        chain_scan = ['https://polygonscan.com/'];
         break;
       case 80001:
         chain_name = 'Polygon Testnet Mumbai';
+        chain_currency = {
+          name: 'MATIC',
+          symbol: 'MATIC',
+          decimals: 18,
+        };
+        chain_rpc = ['https://rpc-mumbai.matic.today/'];
+        chain_scan = ['https://mumbai.polygonscan.com/'];
         break;
       default:
         active = false;
     }
 
     setAddNetworkActive(active);
-    setChainName(chain_name);
-  }, [addNetworkActive, chainName]);
+    chain_data = active
+      ? {
+          id: CHAIN_ID,
+          name: chain_name,
+          nativeCurrency: chain_currency,
+          rpc: chain_rpc,
+          scan: chain_scan,
+        }
+      : null;
+    setChainData(chain_data);
+  }, [addNetworkActive, chainData]);
 
   const parseError = (error: any) => {
     if (typeof error === 'object' && 'code' in error) {
@@ -83,24 +130,17 @@ export const Connect = () => {
       state: 'active',
       message: 'Switching to Polygon...',
     });
-    if (addNetworkActive && chainName) {
-      const switchParams = [{ chainId: utils.hexStripZeros(utils.hexlify(CHAIN_ID)) }];
+    if (addNetworkActive && chainData) {
+      const switchParams = [
+        { chainId: utils.hexStripZeros(utils.hexlify(chainData.id)) },
+      ];
       const addParams = [
         {
-          chainId: utils.hexStripZeros(utils.hexlify(CHAIN_ID)),
-          chainName: chainName,
-          nativeCurrency: {
-            name: 'MATIC',
-            symbol: 'MATIC',
-            decimals: 18,
-          },
-          rpcUrls: [
-            'https://polygon-rpc.com/',
-            'https://rpc-mainnet.matic.network/',
-            'https://rpc-mainnet.maticvigil.com/',
-            'https://rpc-mainnet.matic.quiknode.pro/',
-          ],
-          blockExplorerUrls: ['https://polygonscan.com/'],
+          chainId: utils.hexStripZeros(utils.hexlify(chainData.id)),
+          chainName: chainData.name,
+          nativeCurrency: chainData.nativeCurrency,
+          rpcUrls: chainData.rpc,
+          blockExplorerUrls: chainData.scan,
         },
       ];
 
@@ -137,7 +177,8 @@ export const Connect = () => {
     return (
       <button
         className='px-4 py-3 rounded-md bg-gray-800 hover:bg-gray-700 duration-300 text-light font-bold'
-        onClick={connectToMetamask}>
+        onClick={connectToMetamask}
+      >
         Connect to MetaMask
       </button>
     );
@@ -146,13 +187,14 @@ export const Connect = () => {
   if (network?.chainId !== CHAIN_ID) {
     return (
       <div className='px-4 py-3'>
-        {addNetworkActive && chainName ? (
+        {addNetworkActive && chainData ? (
           <>
             <p>It looks like your wallet is on the wrong network.</p>
             <div className='mt-2 text-sm'>
               <button
                 className='px-3 py-2 rounded-md bg-gray-800 hover:bg-gray-700 duration-300 text-light text-sm font-bold'
-                onClick={switchAddPolygonNetwork}>
+                onClick={switchAddPolygonNetwork}
+              >
                 Switch to Polygon
               </button>
               {switchChainError ? (
@@ -172,7 +214,8 @@ export const Connect = () => {
                 href='https://quickswap-layer2.medium.com/guide-how-to-set-up-custom-matic-mainnet-rpc-for-metamask-transfer-assets-from-l1-to-l2-to-use-3b1e55ccb5cb'
                 target='_blank'
                 className='underline'
-                rel='noreferrer'>
+                rel='noreferrer'
+              >
                 Learn how here.
               </a>
             </p>
@@ -186,7 +229,8 @@ export const Connect = () => {
                 href='https://quickswap-layer2.medium.com/guide-how-to-set-up-custom-matic-mainnet-rpc-for-metamask-transfer-assets-from-l1-to-l2-to-use-3b1e55ccb5cb'
                 target='_blank'
                 className='underline'
-                rel='noreferrer'>
+                rel='noreferrer'
+              >
                 learn more
               </a>
               ).
