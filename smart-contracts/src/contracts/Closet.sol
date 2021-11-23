4f95bdd21e3b06ac74e4b3bd4429e0dc21754f65
Graham Vasquez <rescus1221@gmail.com>
2021-11-23 14:47:00 -0500

    Removed console from contract, changed testnet name, adjusted gitignore

--- smart-contracts/src/contracts/Closet.sol
+++ smart-contracts/src/contracts/Closet.sol
@@ -7,8 +7,6 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeab
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-import "hardhat/console.sol";
-
 struct Token {
   string name;
   uint cost;
