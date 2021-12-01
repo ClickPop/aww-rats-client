8c00e2c2c6eec6a673b80597131e9ddc10a9d5c5
Graham Vasquez <rescus1221@gmail.com>
2021-12-01 02:52:54 -0500

    Fixed promo mint logic so you can promo mint inactive items

--- smart-contracts/src/config/env.ts
+++ smart-contracts/src/config/env.ts
@@ -2,7 +2,7 @@ import { config } from 'dotenv';
 config();
 
 export const PRIVATE_KEY = process.env.PRIVATE_KEY;
-export const MUMBAI_TESTNET = 'https://rpc-mumbai.maticvigil.com/';
+export const MUMBAI_TESTNET = 'https://matic-mumbai.chainstacklabs.com';
 export const MUMBAI_TESTNET_CHAIN_ID = 80001;
 export const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 export const CLOSET_ADDRESS = process.env.CLOSET_ADDRESS;
