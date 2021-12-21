6ff317d9e2c03d4497d7a9cd0f84c9b5460f2fd6
Sean Metzgar <sean.metzgar@gmail.com>
2021-12-21 17:26:46 -0500

    Enforcing enum types for Filters

--- src/components/closet/ClosetItemList.tsx
+++ src/components/closet/ClosetItemList.tsx
@@ -7,10 +7,38 @@ import { titleCase } from '~/utils/string';
 import type { MouseEvent } from 'react';
 
 export const ClosetItemList = () => {
+  enum FilterShowEnum {
+    All = 'all',
+    Owned = 'owned',
+    Unowned = 'unowned',
+  }
+  enum FilterTypeEnum {
+    All = 'all',
+    Background = 'background',
+    Tail = 'tail',
+    Ears = 'ears',
+    Pet = 'pet',
+    Hand = 'hand',
+    Color = 'color',
+    Markings = 'markings',
+    Head = 'head',
+    Generation = 'generation',
+    Snout = 'snout',
+    Torso = 'torso',
+    Shirt = 'shirt',
+    Eyes = 'eyes',
+    Glasses = 'glasses',
+    Accessory = 'accessory',
+    Hat = 'hat',
+  }
   const { loading, closetPieces, currentRat, sponsoredPieces, ownedItems } =
     useContext(ClosetContext);
-  const [filterShow, setFilterShow] = useState('all');
-  const [filterType, setFilterType] = useState('all');
+  const [filterShow, setFilterShow] = useState<FilterShowEnum>(
+    FilterShowEnum.All,
+  );
+  const [filterType, setFilterType] = useState<FilterTypeEnum>(
+    FilterTypeEnum.All,
+  );
 
   const activeButton =
     'text-white bg-purple-700 border-purple-900 hover:bg-purple-800 hover:text-white';
@@ -19,19 +47,24 @@ export const ClosetItemList = () => {
 
   const clickFilterShow = (event: MouseEvent) => {
     let target = event.target as HTMLButtonElement;
-    let newFilterShow = target.dataset.filterShow?.toString() || 'all';
+    let newFilterShow =
+      (target.dataset.filterShow?.toString() as FilterShowEnum) ||
+      FilterShowEnum.All;
     if (filterShow !== newFilterShow) {
       setFilterShow(newFilterShow);
     }
   };
 
-  const filterShowMethod = (piece: ClosetTokenWithMeta, state: string) => {
+  const filterShowMethod = (
+    piece: ClosetTokenWithMeta,
+    state: FilterShowEnum,
+  ) => {
     switch (state) {
-      case 'unowned':
+      case FilterShowEnum.Unowned:
         return ownedItems[piece.id.toString()] ? false : true;
-      case 'owned':
+      case FilterShowEnum.Owned:
         return ownedItems[piece.id.toString()] ? true : false;
-      case 'all':
+      case FilterShowEnum.All:
       default:
         return true;
     }
@@ -72,31 +105,37 @@ export const ClosetItemList = () => {
           <div className='inline-flex rounded-md shadow-sm' role='group'>
             <button
               onClick={clickFilterShow}
-              data-filter-show='all'
+              data-filter-show={FilterShowEnum.All}
               type='button'
               className={
                 'px-4 py-2 text-sm font-medium border rounded-l-lg ' +
-                (filterShow === 'all' ? activeButton : inactiveButton)
+                (filterShow === FilterShowEnum.All
+                  ? activeButton
+                  : inactiveButton)
               }>
               Show All
             </button>
             <button
               onClick={clickFilterShow}
-              data-filter-show='owned'
+              data-filter-show={FilterShowEnum.Owned}
               type='button'
               className={
                 'px-4 py-2 text-sm font-medium border ' +
-                (filterShow === 'owned' ? activeButton : inactiveButton)
+                (filterShow === FilterShowEnum.Owned
+                  ? activeButton
+                  : inactiveButton)
               }>
               Show Owned
             </button>
             <button
               onClick={clickFilterShow}
-              data-filter-show='unowned'
+              data-filter-show={FilterShowEnum.Unowned}
               type='button'
               className={
                 'px-4 py-2 text-sm font-medium border rounded-r-lg ' +
-                (filterShow === 'unowned' ? activeButton : inactiveButton)
+                (filterShow === FilterShowEnum.Unowned
+                  ? activeButton
+                  : inactiveButton)
               }>
               Show Un-owned
             </button>
@@ -108,12 +147,12 @@ export const ClosetItemList = () => {
             onChange={(selected, action) => {
               if (selected) {
                 if (typeof selected.value === 'string') {
-                  setFilterType(selected.value);
-                } else if (filterType !== 'all') {
-                  setFilterType('all');
+                  setFilterType(selected.value as FilterTypeEnum);
+                } else if (filterType !== FilterTypeEnum.All) {
+                  setFilterType(FilterTypeEnum.All);
                 }
-              } else if (filterType !== 'all') {
-                setFilterType('all');
+              } else if (filterType !== FilterTypeEnum.All) {
+                setFilterType(FilterTypeEnum.All);
               }
             }}
             isClearable
@@ -128,47 +167,48 @@ export const ClosetItemList = () => {
             ? 'opacity-0 pointer-events-none overflow-hidden'
             : 'opacity-1 pointer-events-auto'
         }`}>
-        {Object.values(sponsoredPieces).length > 0 && filterType === 'all' && (
-          <div>
-            <h3 className='mt-4 mb-1 text-white bold capitalize text-xl'>
-              Sponsored {filterType}
-            </h3>
-            <p className='text-white text-sm'>
-              These pieces are from some of our best friends around the
-              internet.
-            </p>
-            <p className='text-white text-sm mb-4'>
-              Want to{' '}
-              <a
-                href='https://docs.google.com/forms/d/e/1FAIpQLSf7Sya6kD5V-qGE9R7GIlZ6VC9_xR2EwsGo3VouvHcpCULCRg/viewform'
-                target='_blank'
-                className='underline'
-                rel='noreferrer'>
-                add a branded piece
-              </a>{' '}
-              to the closet?
-            </p>
+        {Object.values(sponsoredPieces).length > 0 &&
+          filterType === FilterTypeEnum.All && (
+            <div>
+              <h3 className='mt-4 mb-1 text-white bold capitalize text-xl'>
+                Sponsored {filterType}
+              </h3>
+              <p className='text-white text-sm'>
+                These pieces are from some of our best friends around the
+                internet.
+              </p>
+              <p className='text-white text-sm mb-4'>
+                Want to{' '}
+                <a
+                  href='https://docs.google.com/forms/d/e/1FAIpQLSf7Sya6kD5V-qGE9R7GIlZ6VC9_xR2EwsGo3VouvHcpCULCRg/viewform'
+                  target='_blank'
+                  className='underline'
+                  rel='noreferrer'>
+                  add a branded piece
+                </a>{' '}
+                to the closet?
+              </p>
 
-            <div className='grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4'>
-              {Object.values(sponsoredPieces)
-                .sort((a) => (ownedItems[a.id.toString()] ? -1 : 0))
-                .filter((a) => filterShowMethod(a, filterShow))
-                .map((piece) => (
-                  <ClosetItem
-                    key={piece.id.toString()}
-                    piece={piece}
-                    pieceType={
-                      (piece.tokenMeta.attributes.find(
-                        (a) => a.trait_type === 'Piece Type',
-                      )?.value as string) ?? ''
-                    }
-                  />
-                ))}
+              <div className='grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+                {Object.values(sponsoredPieces)
+                  .sort((a) => (ownedItems[a.id.toString()] ? -1 : 0))
+                  .filter((a) => filterShowMethod(a, filterShow))
+                  .map((piece) => (
+                    <ClosetItem
+                      key={piece.id.toString()}
+                      piece={piece}
+                      pieceType={
+                        (piece.tokenMeta.attributes.find(
+                          (a) => a.trait_type === 'Piece Type',
+                        )?.value as string) ?? ''
+                      }
+                    />
+                  ))}
+              </div>
             </div>
-          </div>
-        )}
+          )}
         {Object.entries(piecesByType).map(([pieceType, pieces]) =>
-          filterType === 'all' || filterType === pieceType ? (
+          filterType === FilterTypeEnum.All || filterType === pieceType ? (
             <div key={pieceType}>
               <h3 className='mt-4 mb-1 text-white bold capitalize text-xl'>
                 {pieceType}
