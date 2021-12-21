0548acb590dcf1514e270225c11b3f5c69c116a9
Sean Metzgar <sean.metzgar@gmail.com>
2021-12-21 12:59:55 -0500

    Filters ready for testing

--- src/components/closet/ClosetItemList.tsx
+++ src/components/closet/ClosetItemList.tsx
@@ -1,13 +1,16 @@
 import React, { useContext, useMemo, useState } from 'react';
+import Select from 'react-select';
 import { ClosetItem } from '~/components/closet/ClosetItem';
 import { ClosetContext } from '~/components/context/ClosetContext';
-import { ClosetTokenWithMeta } from '~/types';
+import { ClosetTokenWithMeta, ReactSelectOption } from '~/types';
+import { titleCase } from '~/utils/string';
 import type { MouseEvent } from 'react';
 
 export const ClosetItemList = () => {
   const { loading, closetPieces, currentRat, sponsoredPieces, ownedItems } =
     useContext(ClosetContext);
   const [filterShow, setFilterShow] = useState('all');
+  const [filterType, setFilterType] = useState('all');
 
   const activeButton =
     'text-white bg-purple-700 border-purple-900 hover:bg-purple-800 hover:text-white';
@@ -23,8 +26,9 @@ export const ClosetItemList = () => {
   };
 
   const filterShowMethod = (piece: ClosetTokenWithMeta, state: string) => {
-    console.log(piece);
     switch (state) {
+      case 'unowned':
+        return ownedItems[piece.id.toString()] ? false : true;
       case 'owned':
         return ownedItems[piece.id.toString()] ? true : false;
       case 'all':
@@ -52,32 +56,71 @@ export const ClosetItemList = () => {
     [closetPieces, ownedItems],
   );
 
+  const pieceTypes = useMemo(() => {
+    let pieceTypes = Object.keys(piecesByType).sort();
+    let pieceTypeOptions: ReactSelectOption[] = pieceTypes.map((pieceType) => ({
+      label: titleCase(pieceType),
+      value: pieceType,
+    }));
+    return pieceTypeOptions;
+  }, [piecesByType]);
+
   return (
     <div>
-      <div className='filters'>
-        <div className='inline-flex rounded-md shadow-sm' role='group'>
-          <button
-            onClick={clickFilterShow}
-            data-filter-show='all'
-            type='button'
-            className={
-              'px-4 py-2 text-sm font-medium border rounded-l-lg ' +
-              (filterShow === 'all' ? activeButton : inactiveButton)
-            }>
-            Show All
-          </button>
-          <button
-            onClick={clickFilterShow}
-            data-filter-show='owned'
-            type='button'
-            className={
-              'px-4 py-2 text-sm font-medium border rounded-r-lg ' +
-              (filterShow === 'owned' ? activeButton : inactiveButton)
-            }>
-            Show Owned
-          </button>
+      {!loading.pieces && (
+        <div className='filters flex space-x-4'>
+          <div className='inline-flex rounded-md shadow-sm' role='group'>
+            <button
+              onClick={clickFilterShow}
+              data-filter-show='all'
+              type='button'
+              className={
+                'px-4 py-2 text-sm font-medium border rounded-l-lg ' +
+                (filterShow === 'all' ? activeButton : inactiveButton)
+              }>
+              Show All
+            </button>
+            <button
+              onClick={clickFilterShow}
+              data-filter-show='owned'
+              type='button'
+              className={
+                'px-4 py-2 text-sm font-medium border ' +
+                (filterShow === 'owned' ? activeButton : inactiveButton)
+              }>
+              Show Owned
+            </button>
+            <button
+              onClick={clickFilterShow}
+              data-filter-show='unowned'
+              type='button'
+              className={
+                'px-4 py-2 text-sm font-medium border rounded-r-lg ' +
+                (filterShow === 'unowned' ? activeButton : inactiveButton)
+              }>
+              Show Un-owned
+            </button>
+          </div>
+          <Select
+            className='select-search'
+            options={pieceTypes}
+            placeholder='What type of piece?'
+            onChange={(selected, action) => {
+              if (selected) {
+                if (typeof selected.value === 'string') {
+                  setFilterType(selected.value);
+                } else if (filterType !== 'all') {
+                  setFilterType('all');
+                }
+              } else if (filterType !== 'all') {
+                setFilterType('all');
+              }
+            }}
+            isClearable
+            isSearchable
+          />
         </div>
-      </div>
+      )}
 
       <div
         className={`flex flex-col w-full ${
@@ -85,10 +128,10 @@ export const ClosetItemList = () => {
             ? 'opacity-0 pointer-events-none overflow-hidden'
             : 'opacity-1 pointer-events-auto'
         }`}>
-        {Object.values(sponsoredPieces).length > 0 && (
+        {Object.values(sponsoredPieces).length > 0 && filterType === 'all' && (
           <div>
             <h3 className='mt-4 mb-1 text-white bold capitalize text-xl'>
-              Sponsored
+              Sponsored {filterType}
             </h3>
             <p className='text-white text-sm'>
               These pieces are from some of our best friends around the
@@ -124,25 +167,29 @@ export const ClosetItemList = () => {
             </div>
           </div>
         )}
-        {Object.entries(piecesByType).map(([pieceType, pieces]) => (
-          <div key={pieceType}>
-            <h3 className='mt-4 mb-1 text-white bold capitalize text-xl'>
-              {pieceType}
-            </h3>
+        {Object.entries(piecesByType).map(
+          ([pieceType, pieces]) =>
+            filterType !== 'all' &&
+            filterType === pieceType && (
+              <div key={pieceType}>
+                <h3 className='mt-4 mb-1 text-white bold capitalize text-xl'>
+                  {pieceType}
+                </h3>
 
-            <div className='grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4'>
-              {pieces
-                .filter((a) => filterShowMethod(a, filterShow))
-                .map((piece) => (
-                  <ClosetItem
-                    key={piece.id.toString()}
-                    piece={piece}
-                    pieceType={pieceType}
-                  />
-                ))}
-            </div>
-          </div>
-        ))}
+                <div className='grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+                  {pieces
+                    .filter((a) => filterShowMethod(a, filterShow))
+                    .map((piece) => (
+                      <ClosetItem
+                        key={piece.id.toString()}
+                        piece={piece}
+                        pieceType={pieceType}
+                      />
+                    ))}
+                </div>
+              </div>
+            ),
+        )}
       </div>
     </div>
   );
