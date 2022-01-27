471fb2f4712dc7450d0963f41d738fb1ef88ed7e
chris <chris.vqz@gmail.com>
2022-01-26 23:56:06 -0500

    Add BattleCard component to styleguide

--- src/components/styleguide/Styleguide.tsx
+++ src/components/styleguide/Styleguide.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Button } from '~/components/game/Button';
-import { GameIcon } from '~/types/game';
+import { EncounterType, GameIcon, Rattribute } from '~/types/game';
 import { PickRatButton } from '../game/PickRatButton';
+import { BattleCard } from '../game/BattleCard';
+import battleCardPlaceholder from '~/assets/images/den.png';
 
 export const Styleguide = () => {
   return (
-    <div className='mx-auto max-w-xl py-20 px-4'>
+    <div className='py-20 px-4'>
       <div className='mb-4'>
         <h1 className='text-xl text-white mb-2'>Buttons</h1>
         <div className='flex mt-2'>
@@ -47,6 +49,19 @@ export const Styleguide = () => {
           />
           <PickRatButton ratType={GameIcon.PackRat} label='Pack Rat' mr={2} />
         </div>
+      </div>
+      <div className='my-4'>
+          <BattleCard
+              className='max-w-4xl text-white'
+              imgPath={battleCardPlaceholder}
+              altText='The Battle thubmnail illustration.'
+              title='Cat'
+              description='A description of the encounters you are about to undertake. This should be fun and add to the lore of the rataverse. Are you trying to acquire trash? Get a snack? Help a spider save a life through artistic weaving?'
+              encounterType={EncounterType.Solo}
+              weakness={Rattribute.Cunning}
+              resistance={Rattribute.Cuteness}
+              energy={5}
+          />
       </div>
     </div>
   );
