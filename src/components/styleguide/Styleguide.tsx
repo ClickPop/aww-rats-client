0931d78bce689fd353b9db9011b85f426f816b60
Sean Metzgar <sean.metzgar@gmail.com>
2022-01-27 13:24:00 -0500

    Adds Disabled and Selected Thumb Cards

--- src/components/styleguide/Styleguide.tsx
+++ src/components/styleguide/Styleguide.tsx
@@ -1,67 +1,99 @@
 import React from 'react';
+import { Heading, Flex, Box, VStack, HStack } from '@chakra-ui/react';
+import { Image } from '~/components/shared/Image';
 import { Button } from '~/components/game/Button';
 import { GameIconTypes } from '~/types/game';
-import { PickRatButton } from '../game/PickRatButton';
-import { ThumbCard } from '../game/ThumbCard';
-import { Stat } from '../game/Stat';
+import { PickRatButton } from '~/components/game/PickRatButton';
+import { ThumbCard } from '~/components/game/ThumbCard';
+import { Stat } from '~/components/game/Stat';
 
 import fpoCat from '~/assets/images/ratrace/fpo/fpo.cat.png';
+import fpoBG from '~/assets/images/ratrace/fpo/fpo.bg.jpg';
 
 export const Styleguide = () => {
   return (
-    <div className='mx-auto max-w-xl py-20 px-4'>
-      <div className='mb-4'>
-        <h1 className='text-xl text-white mb-2'>Buttons</h1>
-        <div className='flex mt-2'>
-          <Button mr='2'>Primary</Button>
-          <Button mr='2' buttonType='secondary'>
-            Secondary
-          </Button>
-          <Button mr='2' buttonType='destructive'>
-            Destructive
-          </Button>
-        </div>
+    <Box w='100vw' minH='100vh' pos='fixed' top={0} left={0} color='dark.500'>
+      <Image src={fpoBG} alt='background' objectFit='contain' className='absolute top-0 left-0 w-max h-max' />
+      <VStack maxW={800} mx='auto' pt={40} pos='relative' zIndex={2}>
+          <Box w='100%' p={6} rounded="md" bg='whiteAlpha.600' boxShadow="md">
+            <Heading as='h2' size='lg' mb={2}>Buttons</Heading>
+            
+            <Flex mt={2}>
+              <Button mr={2}>Primary</Button>
+              <Button mr={2} buttonType='secondary'>
+                Secondary
+              </Button>
+              <Button mr={2} buttonType='destructive'>
+                Destructive
+              </Button>
+            </Flex>
 
-        <div className='flex mt-2'>
-          <Button mr='2' icon={GameIconTypes.Energy} iconNumber={5}>
-            Primary
-          </Button>
-          <Button
-            mr='2'
-            icon={GameIconTypes.Energy}
-            iconNumber={5}
-            buttonType='secondary'>
-            Secondary
-          </Button>
-          <Button
-            mr='2'
-            icon={GameIconTypes.Energy}
-            iconNumber={5}
-            buttonType='destructive'>
-            Destructive
-          </Button>
-        </div>
+            <Flex mt={2}>
+              <Button mr={2} icon={GameIconTypes.Energy} iconNumber={5}>
+                Primary
+              </Button>
+              <Button
+                mr={2}
+                icon={GameIconTypes.Energy}
+                iconNumber={5}
+                buttonType='secondary'>
+                Secondary
+              </Button>
+              <Button
+                mr={2}
+                icon={GameIconTypes.Energy}
+                iconNumber={5}
+                buttonType='destructive'>
+                Destructive
+              </Button>
+            </Flex>
 
-        <div className='flex mt-2'>
-          <PickRatButton ratType={GameIconTypes.PetRat} label='Pet Rat' mr={2} />
-          <PickRatButton ratType={GameIconTypes.LabRat} label='Lab Rat' mr={2} />
-          <PickRatButton ratType={GameIconTypes.StreetRat} label='Street Rat' mr={2} />
-          <PickRatButton ratType={GameIconTypes.PackRat} label='Pack Rat' mr={2} />
-          <PickRatButton label='Any old rat...' mr={2} />
-        </div>
+            <Flex mt={2}>
+              <PickRatButton ratType={GameIconTypes.PetRat} label='Pet Rat' mr={2} />
+              <PickRatButton ratType={GameIconTypes.LabRat} label='Lab Rat' mr={2} />
+              <PickRatButton ratType={GameIconTypes.StreetRat} label='Street Rat' mr={2} />
+              <PickRatButton ratType={GameIconTypes.PackRat} label='Pack Rat' mr={2} />
+              <PickRatButton label='Any old rat...' mr={2} />
+            </Flex>
+          </Box>
 
-        <h1 className='text-xl text-white mt-4 mb-2'>Thumb Cards</h1>
-        <div className='flex mt-2'>
-          <ThumbCard 
-            title='Generic TC'
-            image={fpoCat}
-            flag='24hrs'>
-            <Stat icon={GameIconTypes.Strength} label='Strength' showLabel={true} value={5} />
-            <Stat icon={GameIconTypes.Attack} label='Attack' showLabel={true} value={7} />
-            <Stat icon={GameIconTypes.Reward} label='Reward' showLabel={true} value={10} />
-          </ThumbCard>
-        </div>
-      </div>
-    </div>
+          <Box w='100%' p={6} rounded="md" bg='whiteAlpha.600' boxShadow="md">
+            <Heading as='h2' size='lg' mb={2}>Thumb Cards</Heading>
+
+            <Flex mt={2}>
+              <ThumbCard 
+                title='Generic TC'
+                image={fpoCat}
+                flag='24hrs'>
+                <Stat icon={GameIconTypes.Strength} label='Strength' showLabel={true} value={5} />
+                <Stat icon={GameIconTypes.Attack} label='Attack' showLabel={true} value={7} />
+                <Stat icon={GameIconTypes.Reward} label='Reward' showLabel={true} value={10} />
+              </ThumbCard>
+
+              <ThumbCard 
+                title='Disabled TC'
+                state='disabled'
+                image={fpoCat}
+                flag='24hrs'>
+                <Stat icon={GameIconTypes.Strength} label='Strength' showLabel={true} value={5} />
+                <Stat icon={GameIconTypes.Attack} label='Attack' showLabel={true} value={7} />
+                <Stat icon={GameIconTypes.Reward} label='Reward' showLabel={true} value={10} />
+              </ThumbCard>
+
+              <ThumbCard 
+                title='Selected TC'
+                state='selected'
+                image={fpoCat}
+                flag='24hrs'>
+                <Stat icon={GameIconTypes.Strength} label='Strength' showLabel={true} value={5} />
+                <Stat icon={GameIconTypes.Attack} label='Attack' showLabel={true} value={7} />
+                <Stat icon={GameIconTypes.Reward} label='Reward' showLabel={true} value={10} />
+              </ThumbCard>
+            </Flex>
+          </Box>
+      </VStack>
+
+    </Box>
+        
   );
 };
