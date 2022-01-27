5c65529711a90cad2cd8488be188c430d99791b0
Sean Metzgar <sean.metzgar@gmail.com>
2022-01-27 00:38:39 -0500

    Update styleguide with chakra

--- src/components/styleguide/Styleguide.tsx
+++ src/components/styleguide/Styleguide.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Heading, Flex, Box, VStack, HStack } from '@chakra-ui/react';
 import { Button } from '~/components/game/Button';
 import { EncounterType, GameIconTypes, Rattribute } from '~/types/game';
 import { PickRatButton } from '~/components/game/PickRatButton';
@@ -10,79 +11,82 @@ import fpoCat from '~/assets/images/ratrace/fpo/fpo.cat.png';
 
 export const Styleguide = () => {
   return (
-    <div className='py-20 px-4'>
-      <div className='mb-4'>
+    <VStack maxW={800} mx='auto' pt={40}>
+        <Box w='100%' p={6} border='1px solid' borderColor='whiteAlpha.300'>
+          <Heading as='h2' size='lg' color='white' mb={2}>Buttons</Heading>
+          
+          <Flex mt={2}>
+            <Button mr={2}>Primary</Button>
+            <Button mr={2} buttonType='secondary'>
+              Secondary
+            </Button>
+            <Button mr={2} buttonType='destructive'>
+              Destructive
+            </Button>
+          </Flex>
+
+          <Flex mt={2}>
+            <Button mr={2} icon={GameIconTypes.Energy} iconNumber={5}>
+              Primary
+            </Button>
+            <Button
+              mr={2}
+              icon={GameIconTypes.Energy}
+              iconNumber={5}
+              buttonType='secondary'>
+              Secondary
+            </Button>
+            <Button
+              mr={2}
+              icon={GameIconTypes.Energy}
+              iconNumber={5}
+              buttonType='destructive'>
+              Destructive
+            </Button>
+          </Flex>
+
+          <Flex mt={2}>
+            <PickRatButton ratType={GameIconTypes.PetRat} label='Pet Rat' mr={2} />
+            <PickRatButton ratType={GameIconTypes.LabRat} label='Lab Rat' mr={2} />
+            <PickRatButton ratType={GameIconTypes.StreetRat} label='Street Rat' mr={2} />
+            <PickRatButton ratType={GameIconTypes.PackRat} label='Pack Rat' mr={2} />
+            <PickRatButton label='Any old rat...' mr={2} />
+          </Flex>
+        </Box>
+
+        <Box w='100%' p={6} border='1px solid' borderColor='whiteAlpha.300'>
+          <Heading as='h2' size='lg' color='white' mb={2}>Thumb Cards</Heading>
+
+          <Flex mt={2}>
+            <ThumbCard 
+              title='Generic TC'
+              image={fpoCat}
+              flag='24hrs'>
+              <Stat icon={GameIconTypes.Strength} label='Strength' showLabel={true} value={5} />
+              <Stat icon={GameIconTypes.Attack} label='Attack' showLabel={true} value={7} />
+              <Stat icon={GameIconTypes.Reward} label='Reward' showLabel={true} value={10} />
+            </ThumbCard>
+          </Flex>
+        </Box>
+
+        <Box w='100%' p={6} border='1px solid' borderColor='whiteAlpha.300'>
+          <Heading as='h2' size='lg' color='white' mb={2}>Battle Cards</Heading>
+
+          <Flex mt={2}>
+            <BattleCard
+              className='max-w-4xl text-white'
+              imgPath={fpoCat}
+              altText='The Battle thubmnail illustration.'
+              title='Cat'
+              description='A description of the encounters you are about to undertake. This should be fun and add to the lore of the rataverse. Are you trying to acquire trash? Get a snack? Help a spider save a life through artistic weaving?'
+              encounterType={EncounterType.Solo}
+              weakness={Rattribute.Cunning}
+              resistance={Rattribute.Cuteness}
+              energy={5}
+            />
+          </Flex>
+        </Box>
+    </VStack>
         
-        <h1 className='text-xl text-white mb-2'>Buttons</h1>
-        
-        <div className='flex mt-2'>
-          <Button marginRight={2}>Primary</Button>
-          <Button mr={2} buttonType='secondary'>
-            Secondary
-          </Button>
-          <Button mr={2} buttonType='destructive'>
-            Destructive
-          </Button>
-        </div>
-
-        <div className='flex mt-2'>
-          <Button mr={2} icon={GameIconTypes.Energy} iconNumber={5}>
-            Primary
-          </Button>
-          <Button
-            mr={2}
-            icon={GameIconTypes.Energy}
-            iconNumber={5}
-            buttonType='secondary'>
-            Secondary
-          </Button>
-          <Button
-            mr={2}
-            icon={GameIconTypes.Energy}
-            iconNumber={5}
-            buttonType='destructive'>
-            Destructive
-          </Button>
-        </div>
-
-        <div className='flex mt-2'>
-          <PickRatButton ratType={GameIconTypes.PetRat} label='Pet Rat' mr={2} />
-          <PickRatButton ratType={GameIconTypes.LabRat} label='Lab Rat' mr={2} />
-          <PickRatButton ratType={GameIconTypes.StreetRat} label='Street Rat' mr={2} />
-          <PickRatButton ratType={GameIconTypes.PackRat} label='Pack Rat' mr={2} />
-          <PickRatButton label='Any old rat...' mr={2} />
-        </div>
-
-
-        <h1 className='text-xl text-white mt-4 mb-2'>Thumb Cards</h1>
-
-        <div className='flex mt-2'>
-          <ThumbCard 
-            title='Generic TC'
-            image={fpoCat}
-            flag='24hrs'>
-            <Stat icon={GameIconTypes.Strength} label='Strength' showLabel={true} value={5} />
-            <Stat icon={GameIconTypes.Attack} label='Attack' showLabel={true} value={7} />
-            <Stat icon={GameIconTypes.Reward} label='Reward' showLabel={true} value={10} />
-          </ThumbCard>
-        </div>
-
-        <h1 className='text-xl text-white mt-4 mb-2'>Battle Cards</h1>
-
-        <div className='flex mt-2'>
-          <BattleCard
-            className='max-w-4xl text-white'
-            imgPath={fpoCat}
-            altText='The Battle thubmnail illustration.'
-            title='Cat'
-            description='A description of the encounters you are about to undertake. This should be fun and add to the lore of the rataverse. Are you trying to acquire trash? Get a snack? Help a spider save a life through artistic weaving?'
-            encounterType={EncounterType.Solo}
-            weakness={Rattribute.Cunning}
-            resistance={Rattribute.Cuteness}
-            energy={5}
-          />
-        </div>
-      </div>
-    </div>
   );
 };
