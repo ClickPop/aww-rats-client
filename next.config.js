a4d8a2f8179e48a061fdd5d53b2ed212a05b58f5
Graham Vasquez <rescus1221@gmail.com>
2022-01-30 02:00:18 -0500

    Added a redirect to gcp cloud storage for our closet public items

--- next.config.js
+++ next.config.js
@@ -17,6 +17,24 @@ module.exports = {
         destination: '/default-erc721-token-metadata.json',
         permanent: true,
       },
+      {
+        source: '/closet/images/:path*',
+        destination:
+          'https://storage.cloud.google.com/aww-rats/closet/images/:path*',
+        permanent: false,
+      },
+      {
+        source: '/closet/image-thumbnails/:path*',
+        destination:
+          'https://storage.cloud.google.com/aww-rats/closet/image-thumbnails/:path*',
+        permanent: false,
+      },
+      {
+        source: '/closet/tokens/:path*',
+        destination:
+          'https://storage.cloud.google.com/aww-rats/closet/tokens/:path*',
+        permanent: false,
+      },
     ];
   },
   images: {
