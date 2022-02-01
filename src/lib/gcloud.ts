62cf25723dcc51748f40e3491505fbdcffd36638
Graham Vasquez <rescus1221@gmail.com>
2022-01-31 23:30:36 -0500

    Changed gcloud storage auth method away from keyfile in prod

--- src/lib/gcloud.ts
+++ src/lib/gcloud.ts
@@ -1,5 +1,14 @@
 import { Storage } from '@google-cloud/storage';
+import { GCP_CLIENT_EMAIL, GCP_PRIVATE_KEY } from '~/config/env';
+import fs from 'fs';
 
-export const storage = new Storage({
-  keyFilename: './gcloud-key.json',
-});
+const creds = fs.existsSync('./gcloud-key.json')
+  ? { keyfile: './gcloud-key.json' }
+  : {
+      credentials: {
+        client_email: GCP_CLIENT_EMAIL,
+        private_key: GCP_PRIVATE_KEY,
+      },
+    };
+
+export const storage = new Storage(creds);
