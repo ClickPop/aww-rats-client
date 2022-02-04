ea526ebd56aa341878edc202da09c56c8847e94c
Graham Vasquez <rescus1221@gmail.com>
2022-02-03 23:19:56 -0500

    Added encounters list and selected encounter with data from Hasura

---
+++ src/components/game/SelectedEncounter.tsx
@@ -0,0 +1,25 @@
+import { Box } from '@chakra-ui/react';
+import React, { useContext } from 'react';
+import { GameContext } from '~/components/context/GameContext';
+import { BattleCard } from '~/components/game/BattleCard';
+import { Encounter_Types_Enum } from '~/schema/generated';
+
+export const SelectedEncounter = () => {
+  const { selectedEncounter } = useContext(GameContext);
+  return selectedEncounter ? (
+    <Box maxWidth='50%' mx='auto' mt={10}>
+      <BattleCard
+        image={selectedEncounter.image ?? ''}
+        title={selectedEncounter.name}
+        description={selectedEncounter.description ?? ''}
+        weakness={selectedEncounter.encounter_weaknesses.map((w) => w.weakness)}
+        resistance={selectedEncounter.encounter_resistances.map(
+          (r) => r.resistance,
+        )}
+        energy={selectedEncounter.energy_cost}
+        strength={selectedEncounter.power}
+        encounterType={Encounter_Types_Enum.Solo}
+      />
+    </Box>
+  ) : null;
+};
