7ca70463f343c24857f197ee461e2c412b9ea732
Graham Vasquez <rescus1221@gmail.com>
2022-02-11 23:17:55 -0500

    Added login requirement to game, fixed rat selector image

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -1,22 +1,23 @@
 import type { NextPage } from 'next';
 import { RatRaceLayout } from '~/components/layout/RatRaceLayout';
-import { Button } from '~/components/game/Button';
-import { GameIconTypes } from '~/types/game';
 import { Box } from '@chakra-ui/react';
 import { SoloEncounterList } from '~/components/game/solo-encounters/SoloEncounterList';
 import { GameContextProvider } from '~/components/context/GameContext';
 import { SelectedEncounter } from '~/components/game/SelectedEncounter';
 import { RatSelector } from '~/components/game/rats/RatSelector';
+import AuthCookieRequired from '~/components/access/AuthCookieRequired';
 
 const Home: NextPage = () => {
   return (
     <RatRaceLayout className='min-h-screen rrPage--sewer-green'>
       <Box pt={24}>
-        <GameContextProvider>
-          <SoloEncounterList />
-          <SelectedEncounter />
-          <RatSelector />
-        </GameContextProvider>
+        <AuthCookieRequired>
+          <GameContextProvider>
+            <SoloEncounterList />
+            <SelectedEncounter />
+            <RatSelector />
+          </GameContextProvider>
+        </AuthCookieRequired>
       </Box>
     </RatRaceLayout>
   );
