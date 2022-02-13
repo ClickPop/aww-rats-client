2a511f4e42a4c004748f0a66bbb35ba47e60b448
Graham Vasquez <rescus1221@gmail.com>
2022-02-12 23:38:34 -0500

    Fixed gcp upload

--- src/lib/gcloud.ts
+++ src/lib/gcloud.ts
@@ -2,13 +2,11 @@ import { Storage } from '@google-cloud/storage';
 import { GCP_CLIENT_EMAIL, GCP_PRIVATE_KEY } from '~/config/env';
 import fs from 'fs';
 
-const creds = fs.existsSync('./gcloud-key.json')
-  ? { keyfile: './gcloud-key.json' }
-  : {
-      credentials: {
-        client_email: GCP_CLIENT_EMAIL,
-        private_key: GCP_PRIVATE_KEY,
-      },
-    };
+const creds = {
+  credentials: {
+    client_email: GCP_CLIENT_EMAIL,
+    private_key: GCP_PRIVATE_KEY,
+  },
+};
 
 export const storage = new Storage(creds);
