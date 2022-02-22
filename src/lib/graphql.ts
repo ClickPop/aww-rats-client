d3643db7bab3af33b19dcbf8147dabae716f2cf1
Graham Vasquez <rescus1221@gmail.com>
2022-02-22 18:31:26 -0500

    Added subscriptions for closet

---
+++ src/lib/graphql.ts
@@ -0,0 +1,41 @@
+import { ApolloClient, InMemoryCache, split, HttpLink } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { HASURA_BASE_URL } from '~/config/env';
+import * as ws from 'ws';
+
+const isServer = () => typeof window === 'undefined';
+
+const wsLink = new WebSocketLink({
+  uri: `${HASURA_BASE_URL.replace('https://', 'wss://').replace(
+    'http://',
+    'ws://',
+  )}/v1/graphql`,
+  options: {
+    reconnect: true,
+  },
+  webSocketImpl: isServer() ? ws : undefined,
+});
+
+const httpLink = new HttpLink({
+  uri: `${HASURA_BASE_URL}/v1/graphql`,
+  credentials: 'include',
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
+export const apolloClient = new ApolloClient({
+  link: splitLink,
+  cache: new InMemoryCache(),
+  credentials: 'include',
+});
