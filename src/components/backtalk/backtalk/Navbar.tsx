cb8f51242f2f02b3d9ed00bcd3ef7eb0107edb9f
chris <chris.vqz@gmail.com>
2022-04-03 14:23:38 -0400

    Setup dashboard page for backtalk

---
+++ src/components/backtalk/backtalk/Navbar.tsx
@@ -0,0 +1,27 @@
+import {
+  Box,
+  Stack
+} from '@chakra-ui/react';
+import React from 'react';
+import { Image } from '~/components/shared/Image';
+import BacktalkLogo from 'src/assets/images/backtalk/backtalk-logo.png';
+
+export const Navbar = () => {
+  return (
+    <Box
+      backgroundColor='white'
+      boxShadow='xs'
+      py='0.25rem'
+    >
+      <Stack
+        justify='space-between'
+        maxW='4xl'        
+        mx='auto'
+      >
+        <Box>
+          <Image src={BacktalkLogo} alt='BacktalkLogo' height={46} width={58} />
+        </Box>
+      </Stack>
+    </Box>
+  );
+};
---
+++ src/pages/backtalk/index.tsx
@@ -0,0 +1,91 @@
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Spacer,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+} from '@chakra-ui/react';
+import { LayoutDashboard } from '~/components/backtalk/backtalk/LayoutDashboard';
+import { NextPage } from 'next';
+
+const BacktalkPage: NextPage = () => {
+  return (
+    <LayoutDashboard>
+      <Box
+        maxW='4xl'   
+        mx='auto'
+        py={4}
+      >
+        <Flex
+          align='baseline'
+          my={4}
+        >
+          <Heading
+            size='md'
+          >
+            Surveys
+          </Heading>
+          <Spacer />
+          <Button
+            colorScheme='teal'
+            size='sm'
+            variant='link'
+            >
+              Public Surveys
+          </Button>
+          <Button
+            colorScheme='teal'
+            ml={2}
+            size='sm'
+            >
+              + Survey
+          </Button>
+        </Flex>
+
+        <TableContainer
+          backgroundColor='white'
+          border='1px'
+          borderColor='gray.200'
+          borderRadius={8}
+        >
+          <Table variant='simple'>
+            <Thead>
+              <Tr textTransform='uppercase'>
+                <Th>Name</Th>
+                <Th>Last Response</Th>
+                <Th>Status</Th>
+                <Th>Visibility</Th>
+                <Th isNumeric>Responses</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td>Community Ballot</Td>
+                <Td>10 minutes ago</Td>
+                <Td>Active</Td>
+                <Td>Public</Td>
+                <Td isNumeric>121</Td>
+              </Tr>
+              <Tr>
+                <Td>Marbles on Stream POAP</Td>
+                <Td>01/30/2022</Td>
+                <Td>Active</Td>
+                <Td>Public</Td>
+                <Td isNumeric>9/10</Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </LayoutDashboard>
+  );
+};
+
+export default BacktalkPage;
