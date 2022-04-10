b4959a065ad9b76e10c5c566c88ef6799b536a6a
chris <chris.vqz@gmail.com>
2022-04-10 14:35:51 -0400

    Make question headings on results h3s

--- src/components/backtalk/results/SurveyResultsList.tsx
+++ src/components/backtalk/results/SurveyResultsList.tsx
@@ -25,26 +25,26 @@ export const SurveyResultsList: FC = () => {
             key={wallet}
             mb={4}
             p={4}>
-            <Heading as='h3' color='gray.500' mb={2} size='xs'>
+            <Heading as='h3' color='gray.500' mb={4} size='xs'>
               {format(
                 new Date(created_at),
                 "'📅' eeee, MMMM d, yyyy 'at 🕐' H:mm  (z)",
               )}
             </Heading>
+            <Heading as='h3' color='gray.500' size='xs'>
+              Wallet Address:
+            </Heading>
             <Text mb={3}>
-              <strong>
-                Wallet Address:
-                <br />
-              </strong>{' '}
               {wallet}
             </Text>
             {data?.surveys_by_pk?.questions.map((q) =>
               (question_ids as number[])?.findIndex((qid) => qid === q.id) >
               -1 ? (
-                <Text key={q.id} mt={2}>
-                  <strong>{q.prompt}:</strong>
-                  <br />
-                  <span
+                <>
+                  <Heading as='h3' color='gray.500' size='xs' key={q.id} mt={2}>
+                    {q.prompt}
+                  </Heading>
+                  <Text
                     key={
                       wallet +
                       response_values[
@@ -60,8 +60,8 @@ export const SurveyResultsList: FC = () => {
                         )
                       ]
                     }
-                  </span>
-                </Text>
+                  </Text>
+                </>
               ) : null,
             )}
           </Box>
