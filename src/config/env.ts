d262a337620074ad992205b525063ad5385dfde1
Graham Vasquez <rescus1221@gmail.com>
2022-04-21 23:54:11 -0400

    Fixed non-public env

--- src/config/env.ts
+++ src/config/env.ts
@@ -84,4 +84,4 @@ export const GCP_PRIVATE_KEY = process.env.GCP_PRIVATE_KEY ?? '';
 
 export const AUTH_URL = process.env.AUTH_URL ?? 'http://localhost:5002/auth';
 
-export const FEEDER_ID = process.env.FEEDER_ID ?? '';
+export const FEEDER_ID = process.env.NEXT_PUBLIC_FEEDER_ID ?? '';
