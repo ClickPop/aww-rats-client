dc210c910d2f5f8cd1ebe0ef9cd3e4c71525b301
chris <chris.vqz@gmail.com>
2022-05-22 19:11:11 -0400

    Add 404 image and update styles slightly.

--- apps/backtalk/src/pages/404.tsx
+++ apps/backtalk/src/pages/404.tsx
@@ -1,11 +1,23 @@
-import { Center } from '@chakra-ui/react';
+import { Box, Heading, VStack } from '@chakra-ui/react';
+import { Image } from '~/components/shared/Image';
 import { NextPage } from 'next';
 import { LayoutDashboard } from '~/components/backtalk/LayoutDashboard';
+import fourohfour from '~/assets/svg/404.svg';
 
 const NotFoundPage: NextPage = () => {
   return (
     <LayoutDashboard>
-      <Center>Uh oh, looks like this doesn&apos;t exist...</Center>
+      <VStack>
+        <Box py={12}>
+          <Image
+            src={fourohfour}
+            alt='404'
+          />
+        </Box>
+        <Heading size='lg'>
+          Uh oh, looks like this doesn&apos;t exist.
+        </Heading>
+      </VStack>
     </LayoutDashboard>
   );
 };
