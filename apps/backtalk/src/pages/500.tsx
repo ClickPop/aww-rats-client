34f470a19cf5266d100a8a3783997b08d9202ed7
chris <chris.vqz@gmail.com>
2022-05-22 19:25:02 -0400

    Update 500 error page

--- apps/backtalk/src/pages/500.tsx
+++ apps/backtalk/src/pages/500.tsx
@@ -1,11 +1,18 @@
-import { Center } from '@chakra-ui/react';
+import { Box, Heading, VStack } from '@chakra-ui/react';
+import { Image } from '~/components/shared/Image';
 import { NextPage } from 'next';
 import { LayoutDashboard } from '~/components/backtalk/LayoutDashboard';
+import fivehundred from '~/assets/svg/500.svg';
 
 const ServerErrorPage: NextPage = () => {
   return (
     <LayoutDashboard>
-      <Center>Uh oh... something broke.</Center>
+      <VStack>
+        <Box py={12}>
+          <Image src={fivehundred} alt='404' />
+        </Box>
+        <Heading size='lg'>Uh oh, something went wrong.</Heading>
+      </VStack>
     </LayoutDashboard>
   );
 };
